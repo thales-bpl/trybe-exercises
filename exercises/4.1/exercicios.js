046e3e16efae5d7d3ee9e91fce357bb4cd4672e1
Thales Lima <thales.bplima@gmail.com>
2021-06-23 20:16:33 -0300

    adicionado exercícios do dia - até 4

---
+++ exercises/4.1/exercicios.js
@@ -0,0 +1,62 @@
+// Exercício 1
+
+let a = undefined
+let b = undefined
+
+a= 31
+b=554
+
+// Soma
+let soma = a + b
+console.log(soma)
+
+// Subtração
+let sub = a - b
+console.log(sub)
+
+// Multiplicação
+let mult = a * b
+console.log(mult)
+
+// Divisão
+let div = a / b
+console.log(div)
+
+// Módulo
+let mod = a % b
+console.log(mod)
+
+
+// Exercício 2
+let n1 = undefined
+let n2 = undefined
+
+n1=4
+n2=8
+
+if (n1>n2) {
+    console.log(n1)
+} else {
+    console.log(n2)
+}
+
+
+// Exercício 3
+let m1 = undefined
+let m2 = undefined
+let m3 = undefined
+
+m1=14
+m2=8
+m3=9
+
+if (m1 > m2 && m1 > m3) {
+    console.log(m1)
+} else if (m2 > m1 && m2 > m3) {
+    console.log(m2)
+} else {
+    console.log(m3)
+}
+
+
+// Exercício 4
