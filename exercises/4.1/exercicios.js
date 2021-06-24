2ac49d85d7fa6bf71c6e76d2aeca8d6b0f0eae6e
Thales Lima <thales.bplima@gmail.com>
2021-06-23 22:12:15 -0300

    add ex 11

--- exercises/4.1/exercicios.js
+++ exercises/4.1/exercicios.js
@@ -2,29 +2,23 @@
 
 let a = Number
 let b = Number
-
 a= 31
 b=554
 
-// Soma
 let soma = a + b
 console.log(soma)
 
-// Subtração
-let sub = a - b
-console.log(sub)
+let subtração = a - b
+console.log(subtração)
 
-// Multiplicação
-let mult = a * b
-console.log(mult)
+let multiplicação = a * b
+console.log(multiplicação)
 
-// Divisão
-let div = a / b
-console.log(div)
+let divisão = a / b
+console.log(divisão)
 
-// Módulo
-let mod = a % b
-console.log(mod)
+let módulo = a % b
+console.log(módulo)
 
 
 // Exercício 2
@@ -220,3 +214,45 @@ if (vv > 0 || cp > 0) {
 }
 
 
+// Exercício 11
+let salarioBruto = Number
+salarioBruto = 3000
+
+
+let aliquotaINSS = Number
+if (salarioBruto < 1556.94) {
+    aliquotaINSS = salarioBruto * 0.08
+} else if (salarioBruto >= 1556.94 && salarioBruto < 2594.92) {
+    aliquotaINSS = salarioBruto * 0.09
+} else if (salarioBruto >= 1556.93 && salarioBruto < 5189.82) {
+    aliquotaINSS = salarioBruto * 0.11
+} else {
+    aliquotaINSS = 570.88
+}
+
+let salarioINSS = salarioBruto - aliquotaINSS
+
+let aliquotaIR = Number
+let parcelaIR = Number
+
+if (salarioINSS <= 1903.98) {
+    aliquotaIR = 0;
+    parcelaIR = 0;
+} else if (salarioINSS >= 1903.99 && salarioINSS <= 2826.65) {
+    aliquotaIR = 0.075
+    parcelaIR = 142.80
+} else if (salarioINSS >= 2826.66 && salarioINSS <= 3751.05) {
+    aliquotaIR = 0.15
+    parcelaIR = 354.80
+} else if (salarioINSS >= 3751.06 && salarioINSS <= 4664.68) {
+    aliquotaIR = 0.225
+    parcelaIR = 636.13
+} else if (salarioINSS > 4664.68) {
+    aliquotaIR = 0.275
+    parcelaIR = 869.36
+}
+
+let aliquotaIRCalc = salarioINSS * aliquotaIR
+let IRTotal = aliquotaIRCalc - parcelaIR
+let salarioLiquido = salarioINSS - IRTotal
+console.log(salarioLiquido)
