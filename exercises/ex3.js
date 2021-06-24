5560c68902b15019aef5cd402f9d3757c2a580ef
Thales Lima <thales.bplima@gmail.com>
2021-06-24 19:17:45 -0300

    add ex 4-6

--- exercises/ex3.js
+++ exercises/ex3.js
@@ -1,10 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0
-let divisor = 0
+let divisor = numbers.length
 
-for (index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index]
-    divisor += 1
 }
 
 let média = soma / divisor
