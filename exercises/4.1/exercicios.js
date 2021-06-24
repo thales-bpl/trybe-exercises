bb6dff60cd08e3058151d475c9e7e15e2df07c53
Thales Lima <thales.bplima@gmail.com>
2021-06-23 21:35:58 -0300

    add exs 7-10

--- exercises/4.1/exercicios.js
+++ exercises/4.1/exercicios.js
@@ -125,3 +125,98 @@ switch (lowchessp) {
 }  
 
 // Exercício 7
+let nota = Number
+nota = 101
+
+if (nota >= 90 && nota <= 100) {
+    console.log('Você recebeu um A')
+} else if (nota >= 80 && nota <= 100) {
+    console.log('Você recebeu um B')
+} else if (nota >= 70 && nota <= 100) {
+    console.log('Você recebeu um C')
+} else if (nota >= 60 && nota <= 100) {
+    console.log('Você recebeu um D')
+} else if (nota >= 50 && nota <= 100) {
+    console.log('Você recebeu um E')
+} else if (nota < 50 && nota >= 0) {
+    console.log('Você recebeu um F')
+} else {
+    console.log('Erro')
+}
+
+/* switch (nota) {
+    case nota >= 90:
+        console.log('Você recebeu um A')
+        break;
+    case nota >= 80:
+        console.log('Você recebeu um B')
+        break;
+    case nota >= 70:
+        console.log('Você recebeu um C')
+        break;
+    case nota >= 60:
+        console.log('Você recebeu um D')
+        break;
+    case nota >= 50:
+        console.log('Você recebeu um E')
+        break;
+    case nota < 50:
+        console.log('Você recebeu um F')
+        break;
+    default:
+        console.log('erro')
+} */
+
+
+// Exercício 8
+let p1 = 3
+let p2 = 4
+let p3 = 7
+let ppp = Boolean
+
+let pp1 = p1 % 2
+let pp2 = p2 % 2
+let pp3 = p3 % 2
+
+if (pp1 == 0 || pp2 == 0 || pp3 == 0) {
+    ppp = true
+} else {
+    ppp = false
+}
+console.log(ppp)
+
+
+// Exercício 9
+let k1 = 2
+let k2 = 5
+let k3 = 6
+let kkk = Boolean
+
+let kk1 = k1 % 2
+let kk2 = k2 % 2
+let kk3 = k3 % 2
+
+if (kk1 !== 0 || kk2 !== 0 || kk3 !== 0) {
+    kkk = true
+} else {
+    kkk = false
+}
+console.log(kkk)
+
+
+// Exercício 10
+let cp = Number
+let vv = Number
+cp = 20
+vv = 80
+let ct = cp + cp*(0.2)
+let lucro = vv - ct
+let lucroTotal = lucro*1000
+
+if (vv > 0 || cp > 0) {
+    console.log(lucroTotal)
+} else {
+    console.log('Erro. Encerrando programa(??)')
+}
+
+
