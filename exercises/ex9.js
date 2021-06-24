e138b7fb56a0b2dd40a2af9b6b9717eb6acc641e
Thales Lima <thales.bplima@gmail.com>
2021-06-24 19:35:45 -0300

    add ex 7-9

---
+++ exercises/ex9.js
@@ -0,0 +1,13 @@
+let array = []
+
+for (let index = 1; index <= 25; index += 1) {
+    array.push(index)  
+}
+
+let arrayDiv2 = []
+
+for (let indexD = 0; indexD < 25; indexD += 1) {
+    arrayDiv2.push(array[indexD]/2)
+}
+
+console.log(arrayDiv2)
