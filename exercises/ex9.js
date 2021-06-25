27ad3b8b78eafe4bb3bcdb253612f56848ed2662
Thales Lima <thales.bplima@gmail.com>
2021-06-25 01:09:20 -0300

    otimizado ex9 + add bonus 1 e 2

--- exercises/ex9.js
+++ exercises/ex9.js
@@ -4,10 +4,6 @@ for (let index = 1; index <= 25; index += 1) {
     array.push(index)  
 }
 
-let arrayDiv2 = []
-
-for (let indexD = 0; indexD < array.length; indexD += 1) {
-    arrayDiv2.push(array[indexD]/2)
-}
-
-console.log(arrayDiv2)
+for(let numero of array) {
+    console.log(numero/2);
+    }
