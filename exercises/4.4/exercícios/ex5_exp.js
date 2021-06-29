483f9e14b8138c206002668bb41996d0ff56e2cc
Thales Lima <thales.bplima@gmail.com>
2021-06-28 22:44:36 -0300

    melhorado code do ex5_extra

--- exercises/4.4/exercícios/ex5_exp.js
+++ exercises/4.4/exercícios/ex5_exp.js
@@ -1,8 +1,8 @@
 let info = {
-    personagem: ['Margarida', 'Tio Patinhas', 'Pato Donald', 'Mickey', 'Pluto'],
-    origem: ['Pato Donald', "Christmas on Bear Mountain, Dell's Four Color Comics #178"],
-    nota: ['Namorada do personagem principal nos quadrinhos do Pato Donald', "O último MacPatinhas"],
-    recorrente: ['Sim', "Sim"],
+    personagem: ['Margarida', 'Tio Patinhas', 'Pato Donald', 'Mickey', 'Pateta'],
+    origem: ['Pato Donald', "Christmas on Bear Mountain, Dell's Four Color Comics #178", 'The Adventures of Mickey Mouse', 'Steamboat Willie', "Mickey's Revue"],
+    nota: ['Namorada do personagem principal nos quadrinhos do Pato Donald', "O último MacPatinhas", 'Personagem principal', 'Muito amigo do pato rabugento', 'Outro amigo da turma'],
+    recorrente: ['Sim', "Sim", 'Sim', 'Sim', 'Sim'],
 };
 
 for (let key in info) {
@@ -10,15 +10,15 @@ for (let key in info) {
     for (index = 0; index < info[key].length; index += 1) {
         listKey += (info[key][index]);
         if (index < (info[key].length - 2)) {
-            listKey += ', ';
+            listKey += '; ';
         } else if (index < (info[key].length - 1)) {    
             listKey += ' e ';
         } else {
             listKey += '.';
         }
-        if (info[key][0] === info[key][1]) {
-            listKey = "Ambos recorrentes";
-        }      
+        if ((info[key][index]) === (info[key][info[key].length - index])) {
+            listKey = "Todos " + info[key][index] + '.';
+        }     
     }  
     console.log(listKey);
 }
