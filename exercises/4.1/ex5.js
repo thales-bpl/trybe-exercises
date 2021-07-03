fb498a247226e5ce4bcc3e8a2a36c3403c600e59
Thales Lima <thales.bplima@gmail.com>
2021-07-03 04:59:42 -0300

    refatorado ex antigos (4.1 e 4.2)

--- exercises/4.1/ex5.js
+++ exercises/4.1/ex5.js
@@ -7,16 +7,17 @@ a1 = 30
 a2 = 90
 a3 = 60
 
-if (a1 < 0 || a2 < 0 || a3 < 0) {
-    aCheck = false;
-} else if (a1 + a2 + a3 == 180) {
-    aCheck = true;
-} else {
-    aCheck = false;
-}
+console.log(triangulo(a1, a2, a3))
 
-if (aCheck == true)
-    console.log('Triângulo válido')
-else {
-    console.log('Erro ao plotar o triângulo')
+function triangulo (a1, a2, a3) {
+    if (a1 <= 0 || a2 <= 0 || a3 <= 0) {
+        aCheck = false;
+    } else if (a1 + a2 + a3 == 180) {
+        aCheck = true;
+    }  
+    if (aCheck == true) {
+        return ('Triângulo válido');
+    } else {
+        return ('Erro ao plotar o triângulo');
+    }
 }
