21f06cab686da19a6eacd2045af3a1c717e7919e
Thales Lima <thales.bplima@gmail.com>
2021-07-07 15:29:28 -0300

    add ex da aula 5.1 + time-adm.html

---
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -0,0 +1,13 @@
+document.getElementById('paragrafo').innerText = 'Trabalhando, feliz morando em meu lar e curtindo a vida no meu ritmo <3';
+
+/* let sideSquare = document.getElementsByClassName('main-content')[0];
+sideSquare.style.backgroundColor = 'rgb(76,164,109)'; */
+
+document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+
+
+let centralSquare = document.getElementsByClassName('center-content')[0];
+centralSquare.style.backgroundColor = 'rgb(255,255,255)';
+
+
+document.getElementsByTagName('h1').innerText = 'JavaScript';
