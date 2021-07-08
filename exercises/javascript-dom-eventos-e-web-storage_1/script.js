2c36a23abb790efd3bd1ab24db0a7e57dfbb791f
Thales Lima <thales.bplima@gmail.com>
2021-07-08 01:59:49 -0300

    exs dia 5.1

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,13 +1,38 @@
-document.getElementById('paragrafo').innerText = 'Trabalhando, feliz morando em meu lar e curtindo a vida no meu ritmo <3';
+let mainParagraph = document.getElementById('paragrafo')
+rewriteParagraph(mainParagraph, 'Existence is beautiful, if you let it be. Life is not a question. There does not need to be an answer.');
+function rewriteParagraph(targetParagraph, text) {
+    targetParagraph.innerText = text
+}
 
-/* let sideSquare = document.getElementsByClassName('main-content')[0];
-sideSquare.style.backgroundColor = 'rgb(76,164,109)'; */
-
-document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
-
+let sideSquare = document.getElementsByClassName('main-content')[0];
+greenSquare(sideSquare)
+function greenSquare (square) {
+    square.style.backgroundColor = 'rgb(76,164,109)';
+}
 
 let centralSquare = document.getElementsByClassName('center-content')[0];
-centralSquare.style.backgroundColor = 'rgb(255,255,255)';
+whitefy(centralSquare);
+function whitefy (target) {
+    target.style.backgroundColor = 'white';
+}
 
+let title = document.getElementsByTagName('h1')[0]
+writeTitle(title, 'Exercício 5.1 - JavaScript');
+function writeTitle (target, text) {
+    target.innerText = text
+}
 
-document.getElementsByTagName('h1').innerText = 'JavaScript';
+upperCasefy(mainParagraph)
+function upperCasefy (target) {
+    target.style.textTransform = 'uppercase';
+}
+
+const parag = document.getElementsByTagName('p')
+for (let index = 0; index < parag.length; index += 1) {
+    console.log(document.getElementsByTagName('p')[index].innerText);
+}
+
+for (let index in parag.length) {
+    console.log(document.getElementsByTagName('p')[index].innerText);
+}
+// porque o forloop acima não funiona?? :madface:
