1c051da61f7afe64f516c1eb2f0ce3c4b8398e78
Thales Lima <thales.bplima@gmail.com>
2021-07-29 16:25:08 -0300

    add conteudo 8.1

---
+++ exercises/b08-HOF/8.2-forEach-find-some-every-sort/conteudo/pt1.js
@@ -0,0 +1,88 @@
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+
+function verifyGrades() {
+  for (let i = 0; i < students.length; i += 1) {
+    const student = students[i];
+    if (student.grade >= 60) {
+      student.approved = 'Aprovado';
+    } else {
+      student.approved = 'Recuperação';
+    }
+  }
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
+
+// ---------- // ---------- // ---------- // ---------- //
+// --- FOREACH --- //
+// Outra forma de fazer isso seria usando forEach:
+// Syntax: targetArray.forEach((element, index, entireArray)) => 
+
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+
+function verifyGrades() {
+  students.forEach((student, index) => {
+    if (student.grade >= 60) {
+      students[index].approved = 'Aprovado';
+    } else {
+      students[index].approved = 'Recuperação';
+    }
+  });
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
+
+// Outro exemplo: 
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+let firstMultipleOf5;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 5 === 0) {
+    firstMultipleOf5 = numbers[index];
+    break;
+  }
+}
+
+console.log(firstMultipleOf5);
+// 50
+
+// Agora usando Find:
+
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+console.log(firstMultipleOf5);
+// 50
+
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element, blabla, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', blabla);
+  console.log('Array percorrido:', theEntireArray);
+});
