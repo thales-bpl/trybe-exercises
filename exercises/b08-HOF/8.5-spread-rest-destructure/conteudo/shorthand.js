a9bcddfdf1588eef153410946d2da03564bd698b
Thales Lima <thales.bplima@gmail.com>
2021-08-04 03:25:18 -0300

    add 8.5

---
+++ exercises/b08-HOF/8.5-spread-rest-destructure/conteudo/shorthand.js
@@ -0,0 +1,22 @@
+// ----- shorthand ----- //
+
+const newUser = (id, name, email) => {
+  return {
+    id: id,
+    name: name,
+    email: email,
+  };
+};
+
+console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
+
+// podemos simplesmente substituir 'id: id' por 'id' que o Javascript entende que você quer atribuir o valor de id a uma propriedade com o mesmo nome que o parâmetro passado:
+const newUser = (id, name, email) => {
+  return {
+    id,
+    name,
+    email,
+  };
+};
+
+console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
